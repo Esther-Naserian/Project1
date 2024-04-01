9a4be50a1799cacef2e3d72e9fd4cfcd76e1a16d
Esther-Naserian <naseriansamuria@gmail.com>
2024-04-01 22:40:37 +0300

    add files

--- Script.js
+++ Script.js
@@ -1,4 +1,15 @@
 function onButtonClick(){
     alert('ButtonClick')
 }
+const button = document.querySelector('button')
+button.addEventListener('click', onButtonClick (),);
+const newButton = document.createElement('button')
+newButton = textContent = 'more information'
+document.body.appendChild(newButton);
+
+newButton.addEventListener('click' ,() => {
+alert('New button cliccked!')
+})
+
+
     
